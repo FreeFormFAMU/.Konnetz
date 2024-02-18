856858a9f1876ca27c65eba3c155e45555dc24a8
Malachi <malachiwhite31@gmail.com>
2024-02-18 12:54:54 -0500

    project-set up
    Initial comment !!!

---
+++ connetz/src/main/java/com/connetz/connetz/ConnetzApplication.java
@@ -0,0 +1,13 @@
+package com.connetz.connetz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnetzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnetzApplication.class, args);
+    }
+
+}
---
+++ connetz/src/test/java/com/connetz/connetz/ConnetzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connetz.connetz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnetzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
