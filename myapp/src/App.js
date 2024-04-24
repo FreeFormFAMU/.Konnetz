075dedfd0380d183c37fae8968485079d9260728
Zorangewonder <126300877+Zorangewonder@users.noreply.github.com>
2024-04-23 21:39:48 -0400

    Added Menu and Footer

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./Webpages/pages/Home"
 import React from "react"
+import Menu from "./Webpages/pages/fragments/Menu"
+import Footer from "./Webpages/pages/fragments/Footer"
 
 import{
   BrowserRouter as Router,
@@ -12,9 +14,13 @@ function App()
 {
   return(
       <Router>
+          <div className="container">
+              <Menu />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+              </div>
+          <Footer />
       </Router>
   );
 }
---
+++ myapp/src/Webpages/pages/fragments/Menu.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+
+import {Link} from "react-router-dom";
+import logo from "../images/logo.png"
+
+function Menu(props)
+{
+
+
+    const [categories, setCategories] = useState([]);
+    const [username, setUsername] = useState("jane_doe_222335");
+
+//use Effect added below variables
+    useEffect(() =>{
+        const getCategories = async () => {
+            await axios.get("https://localhost:8080/api/category/").then((response) => {
+                setCategories(response.data.categories);
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+
+        getCategories().then(r => {
+            console.log(r);
+        });
+
+    },[])//empty array included so request only runs once
+
+
+    return(
+        <header>
+            <div className="p-3 text-center bg-white border-bottom">
+                <div className="container">
+                    <div className="row">
+
+                        <div className="col-md-4 d-flex justify-content-center justify-content-md-start mb-3 mb-md-0">
+                            <Link className="ms-md-2" to="/">
+                                <img src={logo} height="55" alt="logo"/>
+                            </Link>
+                        </div>
+
+
+                        <div className="col-md-4">
+                            <form className="d-flex input-group w-auto my-auto mb-3 mb-md-0">
+                                <input autoComplete="off" type="search" className="form-control rounded"
+                                       placeholder="Search"/>
+                                <span className="input-group-text border-0 d-none d-lg-flex"><i
+                                    className="fas fa-search"></i></span>
+                            </form>
+                        </div>
+
+
+                        <div
+                            className="col-md-4 d-flex justify-content-center justify-content-md-end align-items-center">
+                            <div className="d-flex">
+
+                                <div className="dropdown">
+                                    <a className="text-reset dropdown-toggle d-flex align-items-center hidden-arrow"
+
+                                       id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown"
+                                       aria-expanded="false">
+                                        {username}
+                                    </a>
+                                    <ul className="dropdown-menu dropdown-menu-end"
+                                        aria-labelledby="navbarDropdownMenuLink">
+                                        <li><a className="dropdown-item">My Posts</a></li>
+                                        <li><a className="dropdown-item">My profile</a></li>
+                                        <li><a className="dropdown-item">Logout</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+
+                <div className="container justify-content-center justify-content-md-between">
+
+                    <ul className="navbar-nav flex-row">
+                        {
+                            categories.map((category, idx) => {
+                                let content = encodeURIComponent(category.content);
+                                return (
+                                    <li className="nav-item me-2 me-lg-0 d-none d-md-inline-block" key={idx}>
+                                        <Link className="nav-link"
+                                              to={`/${category.slug}?t=${category.title}&c=${content}`}>{category.title}</Link>
+                                    </li>
+                                )
+                            })
+                        }
+
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Menu;
