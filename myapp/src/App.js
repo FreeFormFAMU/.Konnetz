154f6b71a13f674d538cca7b6d8187f5455d1c9c
Zorangewonder <126300877+Zorangewonder@users.noreply.github.com>
2024-04-23 21:17:01 -0400

    Added Home page basis on JavaScript
    Yessir

---
+++ myapp/src/App.js
@@ -0,0 +1,22 @@
+import Home from "./Webpages/pages/Home"
+import React from "react"
+
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+function App()
+{
+  return(
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
---
+++ myapp/src/Webpages/pages/Home.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import axios from "axios"
+
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {posts: null};
+    }
+
+    componentDidMount = async() => {
+    const url = "http://localhost:8080/api/post/"
+    try{
+        const response = await axios.get(url);
+        this.setState({posts: response.data.posts });
+    } catch(err) {
+    console.error(err);
+    }
+}
+    render() {
+        return (
+            <div className="col-12">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="mt-3 display-3">Latest Posts</h1>
+                    </div>
+                </div>
+                {this.state.posts ? (
+                    this.state.posts.map((post) => {
+                        const categories = post.categoryId?.map((category, idx) => category.title); // Use optional chaining
+
+                        return (
+                            <div className="card shadow-lg p-3 mb-4" key={post.postId}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{post.title}</h5>
+                                    <h3>Testing</h3>
+                                    <h6 className="card-subtitle mb-2 text-muted">by {post.authorId.username}</h6>
+                                    <p className="card-text">{post.summary}</p>
+                                    <p className="mb-1"><small>Category: {categories.join(", ")}</small></p>
+                                    <p className="mb-1"><small>tags: {post.tags.map((tag, idx) => (
+                                        <span className="badge bg-secondary me-2" key={idx}>{tag}</span>
+                                    ))}</small></p>
+                                </div>
+                            </div>
+                        );
+                    })
+                ) : (
+                    "Loading..."
+                )}
+            </div>
+        );
+    }
+}
+
+export default Home;
