adf2b30d4311bf1333c1542ec5234f477a56aaa4
Zorangewonder <126300877+Zorangewonder@users.noreply.github.com>
2024-04-24 23:46:36 -0400

    Look a little better

--- myapp/src/Webpages/pages/fragments/PostSummaryCard.js
+++ myapp/src/Webpages/pages/fragments/PostSummaryCard.js
@@ -6,20 +6,31 @@ let test = "5IvxgfV9IMqkCIy5o9L2"
 
 function PostSummaryCard({post}) {
 
+    function getDateTime(object){
+        const totalMilliseconds = object.seconds * 1000 + Math.floor(object.nanos / 1000000);
+        const date = new Date(totalMilliseconds);
+        return date.toLocaleString();
+    }
     const categories = post.categoryId?.map((category, idx) => category.title); // Use optional chaining
     //<p className="mb-1"><small>./Category: {categories.join(", ")}</small></p>
     //<h6 className="card-subtitle mb-2 text-muted">by {post.user_id.username}</h6>
     return (
-        //<div className="card shadow-lg p-3 mb-4" key={post.post_id}>
+        <div className="card shadow-lg p-3 mb-4" key={post.post_id}>
+
+
 
         <div className="card-body">
             <h5 className="card-title"><Link to={`/post/5IvxgfV9IMqkCIy5o9L2`}>{post.content}</Link></h5>
 
             <p className="card-text">{post.content}</p>
 
+            <p className="card-text">Created At: {getDateTime(post.created_at)}</p>
+
+            <p className="card-text">Last Updated: {getDateTime(post.updated_at)}</p>
+
 
         </div>
-        //</div>
+        </div>
     );
 
 
